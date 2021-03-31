3375f95398e65cac2bfcf8ee61032251d96a9ef7
Dennis van Malsen <dennis.van.malsen@student.nhlstenden.com>
2021-03-31 15:44:34 +0200

    Update van de files

---
+++ MapNieuw/OkMooi/OkMooi/Pages/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OkMooi.Pages
+{
+    public class DBRepos
+    {
+               public IDbConnection Connect()
+        {
+            string connectionString = @"Server=127.0.0.1;Port=3306;Database=hoekstrafotografie;Uid=root;Pwd='';";
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
