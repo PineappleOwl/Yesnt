2599931326a0ba3861e3ea233bc8f20555eaf746
Dennis van Malsen <dennis.van.malsen@student.nhlstenden.com>
2021-04-01 15:27:33 +0200

    Connectie Database in class.cs/ Index.html.cs

--- MapNieuw/OkMooi/OkMooi/Pages/Class.cs
+++ MapNieuw/OkMooi/OkMooi/Pages/Class.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.Graph;
+using Dapper;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace OkMooi.Pages
 {
-    public class DBRepos
+    public class Class : PageModel
     {
-               public IDbConnection Connect()
+        public IDbConnection IDbConnection { get; private set; }
+
+        public IDbConnection Connect()
         {
-            string connectionString = @"Server=127.0.0.1;Port=3306;Database=hoekstrafotografie;Uid=root;Pwd='';";
-            return new MySqlConnection(connectionString);
+            string connectionstring = @"Server=127.0.0.1;
+                                        Port=3306;
+                                        Database=mydb;
+                                        Uid=root;
+                                        Pwd=;";
+
+            return new MySqlConnection(connectionstring);
+        }
+
+
+
+
+        public List<Tables> Get()
+        {
+            using var connection = Connect();
+            var Tables = connection
+                .Query<Tables>("SELECT * FROM Tables");
+            return (List<Tables>)Tables;
+        }
+
+
+
+        public bool AddSimple(Todo todo)
+        {
+            using var connection = Connect();
+            int numRowEffected = connection.Execute(
+                @"INSERT INTO Todo (Description, Done) VALUES (@Description, @Done)"
+                , todo);
+            return numRowEffected == 1;
         }
     }
 }
--- MapNieuw/OkMooi/OkMooi/Pages/Index.cshtml.cs
+++ MapNieuw/OkMooi/OkMooi/Pages/Index.cshtml.cs
@@ -1,25 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.Graph;
+using Dapper;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace OkMooi.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public IDbConnection IDbConnection { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IDbConnection Connect()
         {
-            _logger = logger;
+            string connectionstring = @"Server=127.0.0.1;
+                                        Port=3306;
+                                        Database=mydb;
+                                        Uid=root;
+                                        Pwd=;";
+
+            return new MySqlConnection(connectionstring);
         }
 
-        public void OnGet()
-        {
 
+
+
+        public List<Tables> Get()
+        {
+            using var connection = Connect();
+            var Tables = connection
+                .Query<Tables>("SELECT * FROM Tables");
+            return (List<Tables>)Tables;
+        }
+
+
+
+        public bool AddSimple(Todo todo)
+        {
+            using var connection = Connect();
+            int numRowEffected = connection.Execute(
+                @"INSERT INTO Todo (Description, Done) VALUES (@Description, @Done)"
+                , todo);
+            return numRowEffected == 1;
         }
     }
 }
