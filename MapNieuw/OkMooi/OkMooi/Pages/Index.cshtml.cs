1faa04fc3f2e83dfc732949bd96053b98b8456ef
PineappleOwl <jonathanoldescheper@hotmail.nl>
2021-04-07 13:11:06 +0200

    no message

--- MapNieuw/OkMooi/OkMooi/Pages/Index.cshtml.cs
+++ MapNieuw/OkMooi/OkMooi/Pages/Index.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using OkMooi.Pages;
 
 
 
@@ -12,6 +15,8 @@ namespace OkMooi.Pages
     {
 
         
+        
+            
 
         [BindProperty] 
         public User NewUser { get; set; } 
@@ -50,6 +55,8 @@ namespace OkMooi.Pages
                 var loggedinUser = new Repository().LogInUser(NewUser);
                 if(loggedinUser)
                 {
+                    
+                    HttpContext.Session.SetString("LoginSession", "Yesnt");
                     return RedirectToPage("Kloot");
                 }
                 else
--- MapNieuw/OkMooi/OkMooi/Pages/Kloot.cshtml.cs
+++ MapNieuw/OkMooi/OkMooi/Pages/Kloot.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,15 @@ namespace OkMooi.Pages
 {
     public class Kloot : PageModel
     {
+
+        [BindProperty] public string aars { get; set; }
+        
         public void OnGet()
         {
+            string buttonpoep = HttpContext.Session.GetString("LoginSession");
+            aars = buttonpoep;
         }
+
+        
     }
 }
