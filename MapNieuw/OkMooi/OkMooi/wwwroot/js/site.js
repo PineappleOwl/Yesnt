bd20bb8260211b6ffdb0d67208b829b06a142199
Dennis van Malsen <dennis.van.malsen@student.nhlstenden.com>
2021-04-09 14:31:20 +0200

    Zoekbalk werkende zover het kan.

--- MapNieuw/OkMooi/OkMooi/wwwroot/js/site.js
+++ MapNieuw/OkMooi/OkMooi/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function() {
+    const searchtext = $("#search").val().trim();
+    query.searchname = searchtext;
+    // post request the necessary data
+    $.post("/collection/search", query, data => {
+        // update the robotlist
+        robotList = data;
+    });
+}
